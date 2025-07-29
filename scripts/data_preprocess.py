fe6639caa80689e255185942b2b0a91b8639ee08
Space0code <bozak.tomi@gmail.com>
2025-07-29 07:00:02 +0200

    basic data preprocessing finished

--- scripts/data_preprocess.py
+++ scripts/data_preprocess.py
@@ -1,53 +1,120 @@
+import os
 import pandas as pd
 
-filename = "data/raw-one-format/cog-load/s_001_user_2024-08-08$14-22-19-252173_2024-08-08$15-20-22-460601.csv"
-df = pd.read_csv(filename, sep=",")
-dfOG = df.copy()
+SOURCE_DATA_DIR_ROOT = "data/raw-one-format/"
+DEST_DATA_DIR_ROOT = "data/processed/"
+source_data_dirs = {
+    "cog-load": os.path.join(SOURCE_DATA_DIR_ROOT, "cog-load/"),
+    "hci-tagging": os.path.join(SOURCE_DATA_DIR_ROOT, "hci-tagging/"),
+}
 
-# filter the columns, preserve only time-rel-seconds, x-avg, y-avg and confidence-left, confidence-right
-df = dfOG[["time-rel-seconds", "x-avg", "y-avg", "confidence-gaze-left", "confidence-gaze-right"]]
+def preprocess_dir(dir_name, source_dir_path, dest_data_dir):
+    """
+    Preprocess the data in the given directory.
+    This function is not used in this script but can be useful for future extensions.
+    """
+    for file in os.listdir(source_dir_path):
+        if file.endswith(".csv"):
+            print(f"Processing file: {file}")
+            filename = os.path.join(source_dir_path, file)
+            preprocess_file(dir_name, file, filename, dest_data_dir)
+        # elif is directory, recursion
+        elif os.path.isdir(os.path.join(source_dir_path, file)):
+            preprocess_dir(dir_name, os.path.join(source_dir_path, file), dest_data_dir)
 
-# rows that have both confidence = 0, put x-avg and y-avg to float('nan')
-df.loc[(df["confidence-gaze-left"] == 0) & (df["confidence-gaze-right"] == 0), ["x-avg", "y-avg"]] = float('nan')
+def preprocess_file(dir_name, filename, file_path, dest_data_dir):
 
-# drop all rows until the first row with confidence > 0
-first_valid_index = df[(df["confidence-gaze-left"] > 0) | (df["confidence-gaze-right"] > 0)].index[0]
-last_valid_index = df[(df["confidence-gaze-left"] > 0) | (df["confidence-gaze-right"] > 0)].index[-1]
-df = df.iloc[first_valid_index:last_valid_index + 1].reset_index(drop=True)
+    df = pd.read_csv(file_path)
+    dfOG = df.copy()
 
-df["time-rel-seconds"] = df["time-rel-seconds"] - df["time-rel-seconds"].min()
+    MANDATORY_COLUMNS = [
+        "time-rel-seconds",
+        "x-avg",
+        "y-avg",
+        "confidence-gaze-left",
+        "confidence-gaze-right",
+    ]
+    # check if all mandatory columns are present
+    for col in MANDATORY_COLUMNS:
+        if col not in df.columns:
+            raise ValueError(f"Mandatory column '{col}' is missing in the file: {file_path}")
 
-df_to_compare = df.copy()
-
-# interpolate missing values in x-avg and y-avg but only a few points in each direction (limit=30 => max 1s window interpolation)
-df["x-avg"] = df["x-avg"].interpolate(method="linear", limit_direction="both", limit=30, limit_area="inside")
-df["y-avg"] = df["y-avg"].interpolate(method="linear", limit_direction="both", limit=30, limit_area="inside")
-
-# smooth out the x-avg and y-avg using a rolling window 
-df["x-avg"] = df["x-avg"].rolling(window=3, min_periods=2, center=True).mean()
-df["y-avg"] = df["y-avg"].rolling(window=3, min_periods=2, center=True).mean()
-
-# plot comparision of original and processed data
-import matplotlib.pyplot as plt
-plt.figure(figsize=(12, 6))
-plt.plot(df_to_compare["time-rel-seconds"], df_to_compare["x-avg"], label="Original x-avg", alpha=0.5)
-plt.plot(df["time-rel-seconds"], df["x-avg"], label="Processed x-avg", color='orange')
-plt.title("X Average Comparison")
-plt.xlabel("Time (seconds)")
-plt.ylabel("X Average")
-plt.legend()
-plt.tight_layout()
-plt.show()
-
-# normalize x-avg and y-avg 
-screen_min_x = 0
-screen_max_x = 1920
-screen_min_y = 0
-screen_max_y = 1080
-df["x-avg"] = (df["x-avg"] - screen_min_x) / (screen_max_x - screen_min_x)
-df["y-avg"] = (df["y-avg"] - screen_min_y) / (screen_max_y - screen_min_y)
+    # filter the columns, preserve only time-rel-seconds, x-avg, y-avg and confidence-left, confidence-right
+    df = dfOG[["time-rel-seconds", "x-avg", "y-avg", "confidence-gaze-left", "confidence-gaze-right"]].copy()
 
 
-print(df.head())
+    if "cog-load" in dir_name:
+        # rows that have both confidence = 0, put x-avg and y-avg to float('nan')
+        condition_good = (df["confidence-gaze-left"] == 1) & (df["confidence-gaze-right"] == 1)
+    elif "hci-tagging" in dir_name:
+        # rows that have at least one confidence > 0, put x-avg and y-avg to float('nan')
+        condition_good = (df["confidence-gaze-left"] == 0) & (df["confidence-gaze-right"] == 0)
 
-print(df.describe())
+    df.loc[~condition_good, ["x-avg", "y-avg"]] = float('nan')
+
+    # drop all rows until the first row with confidence > 0
+    first_valid_index = df[condition_good].index[0]
+    last_valid_index = df[condition_good].index[-1]
+    df = df.iloc[first_valid_index:last_valid_index + 1].reset_index(drop=True)
+
+    # drop where time is nan
+    df = df.dropna(subset=["time-rel-seconds"])
+
+    df["time-rel-seconds"] = df["time-rel-seconds"] - df["time-rel-seconds"].min()
+
+    df_to_compare = df.copy()
+
+    # interpolate missing values in x-avg and y-avg but only a few points in each direction (limit=30 => max 1s window interpolation)
+    df["x-avg"] = df["x-avg"].interpolate(method="linear", limit_direction="both", limit=10, limit_area="inside")
+    df["y-avg"] = df["y-avg"].interpolate(method="linear", limit_direction="both", limit=10, limit_area="inside")
+
+    # smooth out the x-avg and y-avg using a rolling window 
+    df["x-avg"] = df["x-avg"].rolling(window=3, min_periods=2, center=True).mean()
+    df["y-avg"] = df["y-avg"].rolling(window=3, min_periods=2, center=True).mean()
+
+
+    stime = 1081.11
+    etime = 1081.42
+
+    print("Sample df_to_compare:")
+    print(df_to_compare.loc[df_to_compare["time-rel-seconds"].between(stime, etime), :])
+    print("Sample df:")
+    print(df.loc[df["time-rel-seconds"].between(stime, etime), :])
+
+    # plot comparision of original and processed data
+    # import matplotlib.pyplot as plt
+    # plt.figure(figsize=(12, 6))
+    # plt.plot(df["time-rel-seconds"], df["x-avg"], label="Processed x-avg", color='orange')
+    # plt.plot(df_to_compare["time-rel-seconds"], df_to_compare["x-avg"], label="Original x-avg", alpha=0.5)
+    # plt.title("X Average Comparison")
+    # plt.xlabel("Time (seconds)")
+    # plt.ylabel("X Average")
+    # plt.legend()
+    # plt.tight_layout()
+    # plt.show()
+
+    # normalize x-avg and y-avg 
+    screen_min_x = df["x-avg"].min()
+    screen_max_x = df["x-avg"].max()
+    screen_min_y = df["y-avg"].min()
+    screen_max_y = df["y-avg"].max()
+    df["x-avg"] = (df["x-avg"] - screen_min_x) / (screen_max_x - screen_min_x)
+    df["y-avg"] = (df["y-avg"] - screen_min_y) / (screen_max_y - screen_min_y)
+
+
+    print("Processed DataFrame:")
+    print(df.head(3))
+    # print(df.describe())
+
+    # save the processed dataframe to a new CSV file
+    dest_filename = os.path.join(dest_data_dir, filename)
+    os.makedirs(dest_data_dir, exist_ok=True)
+    df.to_csv(dest_filename, index=False)
+    print(f"Processed data saved to: {dest_filename}")
+
+
+if __name__ == "__main__":
+    for dir_name, dir_path in source_data_dirs.items():
+        print("\n" + "=" * 50)
+        print(f"Processing directory: {dir_name} at {dir_path}")
+        preprocess_dir(dir_name, dir_path, os.path.join(DEST_DATA_DIR_ROOT, dir_name))
